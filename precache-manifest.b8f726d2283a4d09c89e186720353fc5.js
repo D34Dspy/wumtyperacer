2971f7fca1592cfe195b075cefff15b2f3ef77b4
Domenic Reckruehm <d.reckruehm@gmail.com>
2022-07-09 14:48:08 +0200

    Updates

---
+++ precache-manifest.b8f726d2283a4d09c89e186720353fc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bae2b5420646af2023c7",
+    "url": "/wumtyperacer/static/css/main.7c517028.chunk.css"
+  },
+  {
+    "revision": "bae2b5420646af2023c7",
+    "url": "/wumtyperacer/static/js/main.fd054f0f.chunk.js"
+  },
+  {
+    "revision": "6a2523538cd33760d0f5",
+    "url": "/wumtyperacer/static/js/runtime~main.6556cbc0.js"
+  },
+  {
+    "revision": "211bcbb5d8231e733fde",
+    "url": "/wumtyperacer/static/js/2.e604d25b.chunk.js"
+  },
+  {
+    "revision": "b2871250ed127b512992",
+    "url": "/wumtyperacer/static/js/3.6f35523b.chunk.js"
+  },
+  {
+    "revision": "0db8981a6d48952506b0c574ec204cac",
+    "url": "/wumtyperacer/static/media/UnitedKingdom.0db8981a.png"
+  },
+  {
+    "revision": "6689ec7e1e491496486959b48f13ec38",
+    "url": "/wumtyperacer/index.html"
+  }
+];
